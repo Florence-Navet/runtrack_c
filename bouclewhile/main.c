8d8dbd9838ce24faf326c0af0d04469c2daa6a84
AeRiaSaito <florence.navet@laplateforme.io>
2025-03-31 19:07:19 +0200

    decrementation

--- bouclewhile/main.c
+++ bouclewhile/main.c
@@ -5,13 +5,11 @@ int main()
 {
     int compteur = 10;
 
-    while (compteur != 10)
+    while (compteur >= 0)
     {
         printf("%d\n", compteur);
         compteur--;
     }
-    printf("%d\n", compteur);
 
     return 0;
-
 }
