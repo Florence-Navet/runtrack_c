6813866415b20e52cb70995297fc63210936990d
AeRiaSaito <florence.navet@laplateforme.io>
2025-03-31 18:35:23 +0200

    boucle while

---
+++ bouclewhile/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int compteur = 0;
+    while (compteur < 10) {
+        printf("Bienvenue à la Plateforme !!\n");
+        compteur++;
+    }
+}
