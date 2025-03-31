f90cec8ac0e28714dd65fb4e36f115c93d9df713
AeRiaSaito <florence.navet@laplateforme.io>
2025-03-31 14:20:31 +0200

    function for add

--- calcul/main.c
+++ calcul/main.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
+int main(int argc, char *argv[])
 {
-    double resultat = 0;
-    resultat = 5.0/2.0;
-    printf("5/2 = %1f", resultat);
+  int resultat = 0, nombre1 = 0, nombre2 = 0;
+
+  // On demande les nombres 1 et 2 à l'utilisateur :
+
+  printf("Entrez le nombre 1 : ");
+  scanf("%d", &nombre1);
+  printf("Entrez le nombre 2 : ");
+  scanf("%d", &nombre2);
+
+  // On fait le calcul :
+
+  resultat = nombre1 + nombre2;
+
+  // Et on affiche l'addition à l'écran :
+
+  printf ("%d + %d = %d\n", nombre1, nombre2, resultat);
+
+  return 0;
 }
