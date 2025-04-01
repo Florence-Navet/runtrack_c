e031f8ed3591bdc59046551295b5f8c0efb78ca2
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-01 20:05:34 +0200

    feat; ajout nouveau print pour recuperer valeur pointeur

--- Jour_2/pointeurs_adressememoire/age.c
+++ Jour_2/pointeurs_adressememoire/age.c
@@ -1,12 +1,19 @@
+//pointeur
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, const char * argv[]) {
     int age = 10;
-    int *pointeurSurAge = &age;
 
-    printf("Adresse mémoire du pointeur : %p\n", (void*)pointeurSurAge);
-    printf("Valeur pointée par le pointeur : %d\n", *pointeurSurAge);
+    printf("La valeur de la variable est %d\n", age);
+    printf("L'adresse de la variable est: %p\n", &age);
+    
+    int* pointeur;
+    pointeur = &age;
+
+    printf("La valeur du pointeur est : %p\n", pointeur);
+    printf("La valeur du pointeur est : %d\n", *pointeur);
 
     return 0;
 }
