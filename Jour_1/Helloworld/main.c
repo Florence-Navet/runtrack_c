7f4edb8812ab5bb7f147bb1ad27bb4f83d0061a7
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-01 10:50:51 +0200

    boucle do while

---
+++ Jour_1/Helloworld/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  // Inclusion de la bibliothèque standard pour printf()
+
+int main() {
+    printf("Hello, World!\n");  // Affiche "Hello, World!" suivi d'un retour à la ligne
+    return 0;  // Indique que le programme s'est terminé avec succès
+}
---
+++ Jour_1/dowhile/main.c
@@ -0,0 +1,18 @@
+/// boucle do while
+
+#include <stdio.h>
+
+int main(){
+    int compteur = 10; 
+
+    do
+    {
+        printf("%d\n", compteur);
+        compteur--;
+    }while (compteur != 0);
+
+    return 0;
+}
+
+    
+
