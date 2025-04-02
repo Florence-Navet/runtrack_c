aaf23a5812f2b4f989899bf3949b88d976d009eb
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-02 08:57:35 +0200

    feat: recuperation valeur avec pointeur

--- Jour_2/pointeurs_adressememoire/pointeur_function.c
+++ Jour_2/pointeurs_adressememoire/pointeur_function.c
@@ -1,5 +1,18 @@
 //pointeur function
 
 #include <stdio.h>
-#include <stdlib.h>
+
+void triple(int *nombre)
+{
+    *nombre *= 3; //valeur se trouvant dant le pointeur
+}
+
+int main(int argc, const char * argv[]){
+    int value = 10;
+    int *pointeur = &value;
+
+    triple(pointeur); //je pointeur en paramètre pour récupérer la valeur de value
+    printf("La valeur de value : %d\n", value);
+    return 0;
+}
 
