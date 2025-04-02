44a6e954a72e9ae37e0396bb70b2a9b98d3c17fc
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-02 19:01:36 +0200

    feat : jour 1 job02

--- Jour_1/Job2/my_strlen.c
+++ Jour_1/Job2/my_strlen.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include<string.h>
 
-//Fct qui calcule la longeur d'une chaine de caractere
 
-int my_strler(const char *str) {
+int longueurChaine(const char* chaine);
 
+int main(int argc, const char *argv[])
+{
+    char chaine[] = "Je code alors que j'ai mal au coude !!!";
     int longueur = 0;
 
-    while
+    // recuperer longueur chaine avec fct
+    longueur = longueurChaine(chaine);
+
+    //afficher longueur chaine
+    printf("La chaîne \"%s\" fait %d caractères de long.\n", chaine, longueur);
+
+    return 0;
+}
+
+int longueurChaine(const char* chaine)
+{
+    int nombreDeCaracteres = 0;
+    char caractereActuel = 0;
+    
+    do
+    {
+        /* code */
+        caractereActuel = chaine[nombreDeCaracteres];
+        nombreDeCaracteres++;
+    } 
+    while (caractereActuel != '\0'); //on boucle tant qu'on n'est pas arrivé à '\O'
+    nombreDeCaracteres--; //retirer \0 pour ne par le compter
+    
+    return nombreDeCaracteres;
 }
