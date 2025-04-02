43f666012709569d37a8994918567fc3fe2d8226
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-02 17:15:38 +0200

    change char in a word

--- Jour_2/tableau/change_char.c
+++ Jour_2/tableau/change_char.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[])
-
+/*
 {   
-    char chaine [6];
+    char chaine [6]; // tableau de 6 pour stocker les 6 éléments avec \0
 
+    //initialisation de la chaine
     chaine [0] = 'h';
     chaine [1] = 'e';
     chaine [2] = 'l';
@@ -14,9 +15,30 @@ int main(int argc, const char * argv[])
     chaine [4] = 'o';
     chaine [5] = '\0';
 
+    //affichage de la chaine grâce au %s du printf
     printf("%s\n", chaine);
 
     return 0;
 
        
 }
+*/
+/*
+{
+    char chaine[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    printf("%s", chaine);
+}
+*/
+
+
+{
+    char chaine[] = "Je suis chez Kévin !!!";// taille automatiquement calculée
+
+   
+    chaine[20] = '?'; //onmodifie un caractere
+
+    printf("%s", chaine);
+
+    return 0;
+}    
