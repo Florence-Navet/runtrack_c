7fd4da43d7b0a565ba8af9eb838565e5a1f5c0e2
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-03 15:33:51 +0200

    feat : open file text and write inside

--- Jour_5/fichier_test/main.c
+++ Jour_5/fichier_test/main.c
@@ -10,6 +10,7 @@ int main(int argc, char* argv[])
 
     // Ouverture du fichier en mode "r+" pour lecture et écriture
     fichier = fopen("test.txt", "r+");
+    int age; //declaration variable age
 
     if (fichier != NULL)
     {
@@ -17,7 +18,13 @@ int main(int argc, char* argv[])
         printf("Le fichier a été ouvert avec succès.\n");
 
         // Ajouter du texte dans le fichier
-        fputs("Salut les développeurs\nBienvenue sur OpenClassrooms !", fichier);
+        fputs("Salut les développeurs\nBienvenue sur OpenClassrooms !\n", fichier);
+
+        printf("Quel age avez-vous ?");
+        scanf("%d", &age);
+
+        //On l"ecrit dans le fichier
+        fprintf(fichier, "Le monsieur qui bosse avec avec nous a %d ans\n", age);
 
         // Toujours fermer le fichier après utilisation
         fclose(fichier);
