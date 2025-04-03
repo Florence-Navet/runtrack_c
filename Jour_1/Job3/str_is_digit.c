7ee20d1e831c318b7bc654d50092c471f70c07f7
AeRiaSaito <florence.navet@laplateforme.io>
2025-04-03 08:56:11 +0200

    feat :  boucle et utilisation de str_is_digit

--- Jour_1/Job3/str_is_digit.c
+++ Jour_1/Job3/str_is_digit.c
@@ -1,2 +1,38 @@
+// fonction vérifiant si un chaine ne  contient que des caracteres
+
 #include <stdio.h>
 
+int str_is_digit(const char *str) 
+{
+    int i = 0;
+
+    //verifions si la chaine est vide
+    if (str[i] == '\0')
+    {
+        return 0; 
+    }
+
+    //parcourons la chaine
+    while (str[i] != '\0')
+    {
+        if (str[i] < '0' || str[i] > '9')
+        {
+            return 0; // ce n'est pas un chiffre.
+        }
+        i++;
+    }
+    return 1; // ce sont des chiffres
+}
+
+//Test de la fct
+
+int main()
+{
+    char *caractere1 = "12345";
+    char *caractere2 = "456dc";
+    char *caractere3 = "";
+
+    printf("\"%s\" ne contient que des chiffres ? %d\n", caractere1, str_is_digit(caractere1)); //1
+    printf("\"%s\" ne contient que des chiffres ? %d\n", caractere2, str_is_digit(caractere2)); //0
+    printf("\"%s\" ne contient que des chiffres ? %d\n", caractere3, str_is_digit(caractere3)); //0
+}
