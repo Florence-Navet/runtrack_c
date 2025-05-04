ee2dde075f4daef43a87e82d740f2dabb53fe8bd
enolaworld <enolaworld@gmail.com>
2025-05-04 17:36:43 +0200

    add new file in open_clroom to new lesson

--- open_clroom/main.c
+++ open_clroom/main.c
@@ -4,6 +4,9 @@
 
 int main()
 {
+    /*
+    Ceci est pour les commentaires plus longs
+    */
     printf("Bonjour ma chouquette !\n On va bientot gouter !\n"); // voila pour les print
     printf("Je suis prete pour redemarrer une nouvelle lecon\n Recommenccons du debut !\n");
     return 0;
