ca4c88a905b3367899b8ee5368397343ba7a9776
enolaworld <enolaworld@gmail.com>
2025-05-04 17:35:43 +0200

    feat: new makefile
    feat: change main and add comment long and short

--- open_clroom/main.c
+++ open_clroom/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> // Ajout de cette ligne
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Bonjour ma chouquette !\n On va bientot gouter !\n"); // voila pour les print
+    printf("Je suis prete pour redemarrer une nouvelle lecon\n Recommenccons du debut !\n");
     return 0;
 }
